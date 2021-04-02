a9e2cbf5c8159497116bfbf96d5bc26f97a89c99
Caitlyn Carney <caitlyn.r.carney@gmail.com>
2021-04-02 11:07:49 -0500

    Prepare and Acquire Functions

--- Caitlyn/wrangle.py
+++ Caitlyn/wrangle.py
@@ -1,9 +1,9 @@
 import env
 import pandas as pd
 
-import pandas as pd
 import sklearn.preprocessing
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def get_connection(db, user=env.user, host=env.host, password=env.password):
@@ -65,9 +65,8 @@ def clean_zillow(df):
     applies all the cleaning funcitons previously created
     drops columns
     renames columns'''
-    # assuming null value for pool means no pool
+    # assuming null value for pool and fireplacecnt means none
     df.poolcnt.fillna(0, inplace = True)
-    # assuming null calie for fireplace means no null
     df.fireplacecnt.fillna(0, inplace = True)
     # drop features/rows with more than 50% null values
     df = drop_50_pct_null(df)
@@ -81,11 +80,8 @@ def clean_zillow(df):
     #create new feature house_age
     today = pd.to_datetime('today')
     df['house_age'] = today.year - df['yearbuilt']
-    #create new feature tax_rate which is the monthyl taxes
     df['tax_rate'] = df.taxvaluedollarcnt / df.taxamount
-    # create new feature for log_error_levels
     df['level_of_log_error'] = pd.qcut(df.logerror, q=5, labels=['L1', 'L2', 'L3', 'L4', 'L5'])
-    # create new feature acres
     df['acres'] = df.lotsizesquarefeet/43560
     #drop features
     df = df.drop(['propertycountylandusecode', 'propertyzoningdesc', 
@@ -113,10 +109,18 @@ def clean_zillow(df):
     # change has_heating_system to binary
     df['has_heating_system'] = df.has_heating_system.replace([2.0, 7.0, 24.0, 6.0, 20.0, 13.0, 18.0, 1.0, 10.0, 11.0], 1)
     df['has_heating_system'] = df.has_heating_system.replace('13', '0')
-    # drop all remaining null values
+    df['has_heating_system'] = (df['has_heating_system'] == True ).astype(int)
     # all of these are 1 unit counts
     df.unitcnt.fillna(1, inplace = True)
     df['unitcnt'] = df.unitcnt.replace([2.0, 3.0, 4.0, 6.0], 1)
+    # change has_fireplace to a binary
+    df['has_fireplace'] = df.has_fireplace.replace([2.0, 3.0, 4.0, 5.0], 1)
+    df['has_fireplace'] = df.has_fireplace.replace(0.0, 0)
+    #fix has_pool to int
+    df['has_pool'] = df.has_fireplace.replace(1.0, 1)
+    df['has_pool'] = df.has_fireplace.replace(0.0, 0)
+    # fix unitcnt to int
+    df['unitcnt'] = (df['unitcnt'] == True ).astype(int)
     # replacing null in quality feature with its median range (6)
     df.quality.fillna(6.0, inplace = True)
     # replacing null in square_feet with its median
@@ -135,15 +139,6 @@ def clean_zillow(df):
     df.city.fillna(25218, inplace = True)
      # replacing null in quality feature with its median
     df.zip_code.fillna(96410, inplace = True)
-    # change has_fireplace to a binary
-    df['has_fireplace'] = df.has_fireplace.replace([2.0, 3.0, 4.0, 5.0], 1)
-    df['has_fireplace'] = df.has_fireplace.replace(0.0, 0)
-    #fix has_pool to int
-    df['has_pool'] = df.has_fireplace.replace(1.0, 1)
-    df['has_pool'] = df.has_fireplace.replace(0.0, 0)
-    # change unitcnt and has_heating_system to integers istead of objects
-    df['has_heating_system'] = (df['has_heating_system'] == True ).astype(int)
-    df['unitcnt'] = (df['unitcnt'] == True ).astype(int)
     #drop remaining null values
     df = df.dropna()
     # change la, oc, and vent into int
@@ -152,8 +147,28 @@ def clean_zillow(df):
     df['in_ventura'] = (df['in_ventura'] == True ).astype(int)
     # set index as parcelid
     df = df.set_index('parcelid')
-    # drop random column that came up
-    df = df.drop(['Unnamed: 0'], axis=1)
+    # finish dropping
+    df = df.drop(['Unnamed: 0', 'yearbuilt'], axis=1)
+    # Handle outliers
+    df = df[df.tax_value < 1153326.5]
+    df = df[df.square_feet < 4506.0]
+    df = df[df.acres < 0.665426997245179]
+    # bin some of the large features
+    # bin the square feet
+    df['square_feet_bins'] = pd.cut(df.square_feet, 
+                            bins = [0,500,1000,1500,2000,2500,3000,3500,4000,4600],
+                            labels = [1, 2, 3, 4, 5, 6, 7, 8,9])
+    df['square_feet_bins'] = (df['square_feet_bins']).astype(int)
+    # bin lot square feet
+    df['lot_sqft_bins'] = pd.cut(df.lot_square_feet, 
+                            bins = [0,10000,20000,30000,40000,50000,60000,70000,10000000],
+                            labels = [0, 1, 2, 3, 4, 5, 6, 7])
+    df['lot_sqft_bins'] = (df['lot_sqft_bins']).astype(int)
+    # bin acres
+    df['acre_bins'] = pd.cut(df.acres, 
+                            bins = [0,25,50,75,100,125,150,175],
+                            labels = [0, 1, 2, 3, 4, 5, 6])
+    df['acre_bins'] = (df['acre_bins']).astype(int)
     return df
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -178,31 +193,36 @@ def split_train_validate_test(train, validate, test):
     ''' This function takes in train, validate and test
     splits them into X and y versions
     returns X_train, X_validate, X_test, y_train, y_validate, y_test'''
-    X_train = train.drop(columns = ['appraised_value'])
-    y_train = train.appraised_value
-    X_validate = validate.drop(columns=['appraised_value'])
-    y_validate = validate.appraised_value
-    X_test = test.drop(columns=['appraised_value'])
-    y_test = test.appraised_value
+    X_train = train.drop(columns = ['logerror'])
+    y_train = train.logerror
+    X_validate = validate.drop(columns=['logerror'])
+    y_validate = validate.logerror
+    X_test = test.drop(columns=['logerror'])
+    y_test = test.logerror
     return X_train, X_validate, X_test, y_train, y_validate, y_test
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Scale the Data
 
-scale_columns = ['bathrooms', 'bedrooms', 'quality', 
-                  'square_feet', 'full_bathrooms', 
-                  'latitude', 'longitude', 'lot_square_feet', 'has_pool', 
-                  'land_type', 'city', 'county', 'zip_code', 
-                 'room_count', 'unitcnt', 'structure_tax_value', 
-                 'tax_value',  'land_tax_value', 'taxamount',
-                  'house_age', 'tax_rate']
 
-def scale_my_data(train, validate, test, scale_columns):
+def scale_my_data(train, validate, test):
+    scale_columns = ['bathrooms', 'bedrooms', 'quality', 
+              'square_feet', 'full_bathrooms', 
+              'latitude', 'longitude', 'lot_square_feet',  
+              'land_type', 'city', 'county', 'zip_code', 
+             'room_count', 'unitcnt', 'structure_tax_value', 
+             'tax_value',  'land_tax_value', 'taxamount',
+              'house_age', 'tax_rate', 'acres']
     scaler = MinMaxScaler()
     scaler.fit(train[scale_columns])
-    
+
     train_scaled = scaler.transform(train[scale_columns])
     validate_scaled = scaler.transform(validate[scale_columns])
     test_scaled = scaler.transform(test[scale_columns])
+    #turn into dataframe
+    train_scaled = pd.DataFrame(train_scaled)
+    validate_scaled = pd.DataFrame(validate_scaled)
+    test_scaled = pd.DataFrame(test_scaled)
+    
     return train_scaled, validate_scaled, test_scaled
